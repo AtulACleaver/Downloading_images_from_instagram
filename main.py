7222f1742b651c5646bf242afbfc38bb488b3c53
Atul Anand <atulcleaver@gmail.com>
2021-04-02 08:55:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,94 @@
+import os
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+import time
+
+#specify the path to chromedriver.exe (download and save on your computer)
+driver = webdriver.Firefox()
+
+#open the webpage
+driver.get("http://www.instagram.com")
+
+#target username
+username = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='username']")))
+password = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='password']")))
+
+my_username = os.environ['my_username']
+my_password = os.environ['my_password']
+#enter username and password
+username.clear()
+username.send_keys(my_username)
+password.clear()
+password.send_keys(my_password)
+
+#target the login button and click it
+button = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))).click()
+
+#We are logged in!
+
+time.sleep(5)
+alert = WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Not Now")]'))).click()
+
+#alert2 = WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Not Now")]'))).click()
+
+#target the search input field
+searchbox = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@placeholder='Search']")))
+searchbox.clear()
+
+#search for the hashtag cat
+keyword = "#localhackday"
+searchbox.send_keys(keyword)
+ 
+time.sleep(5) # Wait for 5 seconds
+my_link = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//a[contains(@href, '/" + keyword[1:] + "/')]")))
+my_link.click()
+
+
+#scroll down 2 times
+#increase the range to sroll more
+n_scrolls = 2
+for j in range(0, n_scrolls):
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(5)
+
+#target all the link elements on the page
+anchors = driver.find_elements_by_tag_name('a')
+anchors = [a.get_attribute('href') for a in anchors]
+#narrow down all links to image links only
+anchors = [a for a in anchors if str(a).startswith("https://www.instagram.com/p/")]
+
+print('Found ' + str(len(anchors)) + ' links to images')
+anchors[:5]
+
+
+images = []
+
+#follow each image link and extract only image at index=1
+for a in anchors:
+    driver.get(a)
+    time.sleep(5)
+    img = driver.find_elements_by_tag_name('img')
+    img = [i.get_attribute('src') for i in img]
+    images.append(img[1])
+    
+images[:5]
+
+import wget
+
+path = os.getcwd()
+path = os.path.join(path, keyword[1:] + "s")
+
+#create the directory
+os.mkdir(path)
+
+path
+
+#download images
+counter = 0
+for image in images:
+    save_as = os.path.join(path, keyword[1:] + str(counter) + '.jpg')
+    wget.download(image, save_as)
+    counter += 1
